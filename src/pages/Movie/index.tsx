2b71b882282c26dcb3765fc0c0da8f8ce61f0ffc
si <anhtuanpro6420@gmail.com>
2022-03-11 21:40:40 +0700

    add genre selection

--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -1,19 +1,33 @@
 import React, { FC, useEffect, useState } from 'react';
+import { Select } from 'antd';
 import { getMoviesAPI } from 'apis/movie.api';
 import { IMovie } from 'interfaces/movie.interface';
 import './Movie.scss';
 import Carousel from 'components/Carousel';
 import MovieDetail from 'components/MovieDetail';
 
+const { Option } = Select;
+
 const Movie: FC = () => {
     const [movies, setMovies] = useState([] as Array<IMovie>);
+    const [allOrgMovies, setAllOrgMovies] = useState([] as Array<IMovie>);
     const [selectedMovie, setSelectedMovie] = useState<IMovie | null>(null);
+    const [genres, setGenres] = useState<Array<string>>([]);
+
+    const getGenres = (allMovies: Array<IMovie> = []) => {
+        return Array.from(
+            new Set([...allMovies.map((movie: IMovie) => movie?.Genre)])
+        );
+    };
 
     const fetchMovies = async () => {
         const moviesResponse: Array<IMovie> = await getMoviesAPI();
         const firstMovie: IMovie = moviesResponse[0] || null;
         setMovies(moviesResponse);
+        setAllOrgMovies(moviesResponse);
         setSelectedMovie(firstMovie);
+        const allGenres: Array<string> = getGenres(moviesResponse);
+        setGenres(allGenres);
     };
 
     useEffect(() => {
@@ -24,9 +38,43 @@ const Movie: FC = () => {
         setSelectedMovie(movie);
     };
 
+    const handleSelectGenre = (selectedGenre: string) => {
+        if (selectedGenre === 'all') {
+            setMovies(allOrgMovies);
+        } else {
+            const moviesByGenre: Array<IMovie> = [...allOrgMovies].filter(
+                (movie: IMovie) => movie?.Genre === selectedGenre
+            );
+            console.log(moviesByGenre);
+            setMovies(moviesByGenre);
+        }
+    };
+
+    const renderGenresSelection = () => {
+        return (
+            <Select
+                onChange={handleSelectGenre}
+                placeholder='Gender'
+                className='render-selection'
+            >
+                <Option key='all'>All</Option>
+                {genres.map((genre: string) => (
+                    <Option key={genre} value={genre}>
+                        {genre}
+                    </Option>
+                ))}
+            </Select>
+        );
+    };
+
     return (
         <div className='movie-page'>
             {selectedMovie && <MovieDetail movie={selectedMovie} />}
+            {genres && genres.length > 0 && (
+                <div className='genres-selection-container'>
+                    {renderGenresSelection()}
+                </div>
+            )}
             <Carousel movies={movies} onSelectMovie={handleSelectMovie} />
         </div>
     );
